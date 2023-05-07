a2ada9ee06a0a19ff1add1d508004d450fc85306
Tanya <122279254+robo-bun@users.noreply.github.com>
2023-05-07 20:36:23 +0400

    Update index.php

--- index.php
+++ index.php
@@ -8,46 +8,29 @@
 Car и Motorcycle и использовать методы start() и stop() для моделирования запуска и остановки двигателя.
 
 
-class Venicle {
-    private $speed;
-    public function start() {
-        echo "Запускаю двигатель! ";
+class Car extends Venicle {
+    private int $passengers;
+    public function __construct($passengers) {
+        $this->passengers = $passengers;
     }
-    public function stop() {
-        echo "Торможу!" . "<br>";
+    public function carRides() {
+        echo "Легковой автомобиль c " . $this->passengers . " пассажирами мчит по дороге." . "<br>";
     }
+}
+class Motorcycle extends Venicle {
+    public function helmetWearing($speed) {
+        $speedLimit = 60;
+        echo $speed <= $speedLimit ? "Хорошей дороги :)" : "Остановись и одень шлем!!! ";
+    }
+}
 
-    public function drive($speed) {
-        $this->speed = $speed;
-        echo "Едем со скоростью " . $this->speed . " км/ч. ";
-        }
-    }
-    class Car extends Venicle {
-        private $passengers;
-        public function __construct($passengers) {
-            $this->passengers = $passengers;
-        }
-        public function carRides() {
-            echo "Легковой автомобиль c " . $this->passengers . " пассажирами мчит по дороге." . "<br>";
-        }
-    }
-    class Motorcycle extends Venicle {
-        public function helmetWearing($speed) {
-            if ($speed <= 60) {
-                echo "Хорошей дороги :)";
-            } else if ($speed >= 61) {
-                echo "Остановись и одень шлем!!! ";
-            }
-        }
-    }
+$car = new Car(2);
+$car->start();
+$car->drive(120);
+$car->carRides();
 
-    $car = new Car('2');
-    $car->start();
-    $car->drive('120');
-    $car->carRides();
-
-    $motorcycle = new Motorcycle();
-    $motorcycle->start();
-    $motorcycle->drive('70');
-    $motorcycle->helmetWearing('70');
-    $motorcycle->stop();
+$motorcycle = new Motorcycle();
+$motorcycle->start();
+$motorcycle->drive(70);
+$motorcycle->helmetWearing(70);
+$motorcycle->stop();
